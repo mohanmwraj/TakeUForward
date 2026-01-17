42467fee38d9327597f50ae71c7bf2667b0f2846
Mohan Raj <mohanmwraj@gmail.com>
2026-01-17 11:15:32 +0530

    DSA

--- StacksAndQueues/monotonoicStack/AsteroidCollision.java
+++ StacksAndQueues/monotonoicStack/AsteroidCollision.java
@@ -1,5 +1,73 @@
 package StacksAndQueues.monotonoicStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+  you are given an array of integers asteroids representing asteroids in a row.
+    For each asteroid, the absolute value represents its size, 
+    and the sign represents its direction (positive meaning right, negative meaning left).
+    Each asteroid moves at the same speed.
+    Find out the state of the asteroids after all collisions.
+
+*/
+
+class Soultion{
+
+    /*
+        When two asteroids are moving in opposite direction, they collide. There can be two cases of
+        opposite moving asteroids:
+        1. moving apart from each other - no collision.
+        2. moving towards each other - collision occurs.
+
+        Now in order to perform the collisions, only the left moving asteroids must be observed as all the earlier 
+        asteriods will be moving towards the right direction. 
+        For every moving asteriod, we must first consider the asteriod that was found last moving towards the right because it will
+        be the asteroid undergoing collision first. This gives an idea of using
+        stack data structure to store its elements in LIFO fashion.
+    */
+}
+
 public class AsteroidCollision {
-    
+    public static int[] AsteroidCollision(int[] asteriods){
+        int n = asteriods.length;
+        List<Integer> st = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            int curr = asteriods[i];
+            if(curr > 0){
+                st.add(curr);
+                continue;
+            }
+
+            while(!st.isEmpty() && st.get(st.size() - 1) > 0 
+                    && st.get(st.size() - 1) < Math.abs(curr)){
+                st.remove(st.size() - 1);
+            }
+
+            /*
+                if there is right moving asteroid which is of same size as current left moving asteroid,
+                then both will explode each other and we don't need to add current asteroid to stack. 
+            */
+            if(!st.isEmpty() && st.get(st.size() - 1) == Math.abs(curr)){
+                st.remove(st.size() - 1);
+            } 
+            /*
+                otherwise if there is no left moving asteroid, the right moving asteroid will not be destroyed
+                and we can safely add the current left moving asteroid to stack.
+            */
+            else if(st.isEmpty() || st.get(st.size() - 1) < 0){
+                st.add(curr);
+            }
+        }
+
+        // int[] res = new int[st.size()];
+        // for(int i = 0; i < st.size(); i++){
+        //     res[i] = st.get(i);
+        // }
+
+        return st.stream().mapToInt(Integer::intValue).toArray();
+        
+    }
 }
--- StacksAndQueues/monotonoicStack/NextGreaterElement2.java
+++ StacksAndQueues/monotonoicStack/NextGreaterElement2.java
@@ -1,15 +1,112 @@
 package StacksAndQueues.monotonoicStack;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /*
     Give a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]),
     return an array answer where answer[i] is the next greater number of nums[i].
     If it does not exist, return -1 for this number.
 
-    
+
 
 */
 
+class Solution{
+
+    /*
+        Appproach 1: Brute Force
+        > similar to Next Greater Element 1
+        > for each element, we need to traverse the array twice to find the next greater element
+        > Handling circular Array:
+            one way is to double the array by pushing all the elements of array at the back in order. But this 
+            will take extra space.
+            another way is to use modulo operator to simulate the circular array. This will double the array hypothetically s
+            saving the extra space.
+        > time complexity: O(n^2)
+        > space complexity: O(1)
+    
+    */
+
+    public int[] approach1(int[] nums){
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+
+        for(int i=0; i<n; i++){
+            for(int j=1; j<n; j++){
+                int circularIndex = (i + j) % n; // simulate circular array hypothetical index
+                if(nums[circularIndex] > nums[i]){
+                    result[i] = nums[circularIndex];
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /*
+        Approach 2: Using Monotonic Stack
+        > similar to Next Greater Element 1
+        > we will traverse the array twice to simulate the circular array
+        > Start traversing the hypothetically doubled array from back using the modulus operator. For the current element,
+            pop elements from stack till top is less than or equal to current element.
+        > For the first half of the elements, discard the greatest elements if found. For the second half of the elements, if stack is not empty,
+            the top element is the next greater element.
+        > push the current element onto the stack
+        > time complexity: O(n)
+        > space complexity: O(n)
+
+        2 10 12 1 11 | 2 10 12 1 11 [double the array hypothetically]
+                        ^
+                        stack: 11 1 12 10 2 (->top)
+        when encounter 11: pop 1,2 from stack, next greater is 12, push 11
+                        stack: 11 12 10 2
+
+        when 1 comes later there will be no pop as it is smaller than 11
+    */
+
+    public int[] approach2(int[] nums){
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=2*n-1; i>=0; i--){
+            int circularIndex = i % n; // simulate circular array hypothetical index
+
+            while(!stack.isEmpty() && stack.peek() <= nums[circularIndex]){
+                stack.pop();
+            }
+
+            // for the first half of the traversal, we don't need to set the result
+            if(i < n){
+                if(!stack.isEmpty()){
+                    result[circularIndex] = stack.peek();
+                }
+            }
+
+            stack.push(nums[circularIndex]);
+        }
+
+        return result;
+    }
+}
+
 
 public class NextGreaterElement2 {
-    
+    public static void main(String[] args) {
+        int n = 4;
+        int[] arr = {4,5,2,10,8};
+
+        Solution sol = new Solution();
+        int[] result1 = sol.approach1(arr);
+        System.out.println("Approach 1 - Brute Force:");
+        System.out.println(Arrays.toString(result1));
+
+        int[] result2 = sol.approach2(arr);
+        System.out.println("Approach 2 - Monotonic Stack:");
+        System.out.println(Arrays.toString(result2));
+    }
 }
