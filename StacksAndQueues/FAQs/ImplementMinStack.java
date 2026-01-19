a399c14eb27c00a54858ad8ba43b78f6b89d6264
Mohan Raj <mohanmwraj@gmail.com>
2026-01-19 11:37:37 +0530

    DSA

--- StacksAndQueues/FAQs/ImplementMinStack.java
+++ StacksAndQueues/FAQs/ImplementMinStack.java
@@ -1,5 +1,143 @@
 package StacksAndQueues.FAQs;
 
-public class ImplementMinStack {
+import java.util.Stack;
+
+/*
+
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+
+*/
+class MinStack_approach1{
+    /*
+        In a usual stack data structure, there is no method to get the minimum element present in the stack in constant time.
+        this can be overcome, if all the numbers are stored in pairs with their current minimum element.
+        This way, the stack will not only able to perform the previous methods in constnat time, but also perform the getmin()
+        method in constant time.
     
+    */
+    
+    private Stack<int[]> st;
+
+    public MinStack_approach1() {
+        st = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (st.isEmpty()) {
+            st.push(new int[]{val, val});
+        } else {
+            int currentMin = st.peek()[1];
+            st.push(new int[]{val, Math.min(val, currentMin)});
+        }
+    }
+
+    public void pop() {
+        if (!st.isEmpty()) {
+            st.pop();
+        }
+    }
+
+    public int top() {
+        if (!st.isEmpty()) {
+            return st.peek()[0];
+        }
+        return -1; // or throw exception
+    }
+
+    public int getMin() {
+        if (!st.isEmpty()) {
+            return st.peek()[1];
+        }
+        return -1; // or throw exception
+    }
+}
+
+class MinStack_approach2{
+    /*
+        Approach 2: 
+        Instead of storing pairs or auxiliary stacks, we 
+        store a specially calculated value in the main stack that allows us to track the minimum element efficiently.
+        This approach uses less space compared to storing pairs or using an auxiliary stack.
+
+        Push Operation:
+        When pushing a new value, if it's less than or equal to the current minimum, we push a calculated value (2*val - min) 
+        onto the stack and update the minimum.
+        if the value is greater than the current minimum, we push it directly onto the stack.
+        if the stack is empty, we push the value and set it as the minimum.
+
+        Pop Operation:
+        When popping, if the popped value is less than the current minimum, it indicates that this value was a special calculated value.
+        We then recalculate the previous minimum using the formula (2*min - poppedValue).   
+        if the stack is empty, do nothing. Otherwise, retrieve and pop the top value.
+    
+    */
+    private Stack<Integer> mainStack;
+    private int min;
+
+    public MinStack_approach2() {
+        mainStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (mainStack.isEmpty()) {
+            mainStack.push(val);
+            min = val;
+        } else if (val <= min) {
+            mainStack.push(2 * val - min);
+            min = val;
+        } else {
+            mainStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (mainStack.isEmpty()) {
+            return;
+        }
+        int topValue = mainStack.pop();
+        if (topValue < min) {
+            min = 2 * min - topValue;
+        }
+    }
+
+    public int top() {
+        if (!mainStack.isEmpty()) {
+            int topValue = mainStack.peek();
+            if (topValue < min) {
+                return min;
+            } else {
+                return topValue; // return the top value if minimum is less than the top.
+            }
+        }
+        return -1; // or throw exception
+    }
+
+    public int getMin() {
+        if (!mainStack.isEmpty()) {
+            return min;
+        }
+        return -1; // or throw exception
+    }
+}
+
+public class ImplementMinStack {
+    public static void main(String[] args) {
+        MinStack_approach1 minStack = new MinStack_approach1();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // Returns -3
+        minStack.pop();
+        System.out.println(minStack.top());    // Returns 0
+        System.out.println(minStack.getMin()); // Returns -2
+
+        MinStack_approach2 minStack2 = new MinStack_approach2();
+        minStack2.push(-2);
+        minStack2.push(0);
+        minStack2.push(-3);
+        System.out.println(minStack2.getMin()); // Returns -3
+        minStack2.pop();
+        System.out.println(minStack2.top());    // Returns 0    
+    }
 }
--- StacksAndQueues/FAQs/SlidingWindowMaximum.java
+++ StacksAndQueues/FAQs/SlidingWindowMaximum.java
@@ -1,5 +1,100 @@
 package StacksAndQueues.FAQs;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+    Given an array and an integer k, find the maximum for each and every contiguous subarray of size k.
+    Example:
+    Input: arr[] = {1, 3, -1, -3, 5, 3, 6, 7}, k = 3
+    Output: {3, 3, 5, 5, 6, 7}
+    Explanation: 
+    Window position                Maximum
+    ---------------               -----     
+    [1  3  -1] -3  5  3  6  7       3
+     1 [3  -1  -3] 5  3  6  7       3
+     1  3 [-1  -3  5] 3  6  7       5
+     1  3  -1 [-3  5  3] 6  7       5
+     1  3  -1  -3 [5  3  6] 7       6
+     1  3  -1  -3  5 [3  6  7]      7
+
+*/
+
+class SlidingWindowMaximumHelper {
+    /*
+        Approach 1: Brute Force
+
+        A naive approach is to consider all windows of size k and find the maximum element in each window.
+        This approach has a time complexity of O(n*k), where n is the number of elements in the array.
+        Time Complexity: O((n-k)*k)
+        Space Complexity: O(N-k+1) for the result list
+    */
+    public List<Integer> slidingWindowMaximum_bruteforce(int[] arr, int k) {
+        int n = arr.length;
+        List<Integer> result = new java.util.ArrayList<>();
+        for (int i = 0; i <= n - k; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, arr[j]);
+            }
+            result.add(max);
+        }
+        return result;
+    }
+
+    /*
+        Approach 2: Using Deque (Double Ended Queue)(Monotonic Queue)
+
+        We can use a deque to store indices of array elements. The idea is to maintain the deque such that
+        the elements in it are always in decreasing order. The front of the deque will always have the index
+        of the maximum element for the current window.
+
+        The maximum element in a particular window can be found directly using the concept of monotonic queue, which indicates
+        storing elements in a decreasing order.
+        This way, the front of the deque will always have the maximum element for the current window which can be retrieved
+        from the queue in constant time.
+        In order to maintain the decreasting order, we will remove all the elements smaller than the current element from the back of the deque
+        before adding the current element's index to the deque.
+        But, a queue data structure does not provide pop operation from the front. To overcome this, Deque (Double Ended Queue) can be used,
+        which enable efficient insertion and retrieval from both ends of the queue.
+        
+        Time Complexity: O(n)
+        Space Complexity: O(k) for the deque
+    */
+
+    public List<Integer> slidingWindowMaximum_deque(int[] arr, int k) {
+        int n = arr.length;
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            // Remove elements not in the current window
+            if (!deque.isEmpty() && deque.peekFirst() == i - k) {
+                deque.pollFirst();
+            }
+
+            // Remove elements smaller than the current element from the back of the deque
+            /* Maintain the monotonic decreasing order */
+            while (!deque.isEmpty() && arr[deque.peekLast()] < arr[i]) {
+                deque.pollLast();
+            }
+
+            // Add the current element's index to the deque
+            deque.offerLast(i);
+
+            // The front of the deque is the maximum element for the current window
+            if (i >= k - 1) {
+                result.add(arr[deque.peekFirst()]);
+            }
+        }
+
+        return result;
+    }
+
+}
+
 public class SlidingWindowMaximum {
     
 }
